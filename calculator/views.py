8463130044fa5f16f20c389753b5d7bbddba1a7a
PriyanshBordia <priyanshbordia1@gmail.com>
2021-03-30 00:05:29 +0530

    login

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest, HttpResponseForbidden, Http404
 
 # Create your views here.
+
+def home(request):
+    return render(request, "calculator/home.html")
+
+
+# def login(request):
+#     return render(request, "registration/login.html")
+
+
+def login(request):
+    return render(request, "calculator/home.html")

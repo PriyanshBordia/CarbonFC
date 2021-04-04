641c58b2ed58725018b7dd287c613daaa3833d21
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-05 01:19:31 +0530

    settings

--- CarbonFC/settings.py
+++ CarbonFC/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = default = True
+DEBUG = env('DEBUG') or False
 
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), '127.0.0.1']
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), '127.0.0.1', 'carbonfc.herokuapp.com']
 
 # ADMINS = [env('ADMINS')]
 # ADMINS=(admin@carfon.fc, )

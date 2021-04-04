8458ae055198d25b0cfe775b91ac3c947248f0c3
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-05 00:59:36 +0530

    Update settings.py

--- CarbonFC/settings.py
+++ CarbonFC/settings.py
@@ -10,13 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-import django_heroku
 import environ
 env = environ.Env()
 environ.Env.read_env()
 
 import os
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -209,6 +208,3 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = 'The CarbonFC Team <no-reply@carbon.fc>'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
